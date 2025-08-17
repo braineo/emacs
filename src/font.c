f5e79ebe6f316b85561da72fd2bc40eaeb038f8f
Binbin Ye <phantom2501@gmail.com>
2025-08-17 21:11:47 +0900

    only set features when set

--- src/font.c
+++ src/font.c
@@ -3484,9 +3484,9 @@ font_load_for_lface (struct frame *f, Lisp_Object *attrs, Lisp_Object spec)
       if (STRINGP (name)) font_put_extra (entity, QCuser_spec, name);
 
       /* Store font features from face attributes for use during shaping */
-      if (!UNSPECIFIEDP (attrs[LFACE_FONT_FEATURES_INDEX])
-          && !NILP (attrs[LFACE_FONT_FEATURES_INDEX]))
-        font_put_extra (entity, QCfont_features, attrs[LFACE_FONT_FEATURES_INDEX]);
+      Lisp_Object font_features = attrs[LFACE_FONT_FEATURES_INDEX];
+      if (CONSP (font_features))
+          font_put_extra (entity, QCfont_features, font_features);
     }
   return entity;
 }
